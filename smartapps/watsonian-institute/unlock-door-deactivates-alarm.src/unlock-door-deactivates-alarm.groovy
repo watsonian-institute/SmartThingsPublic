23ffbc4231b2747eb0ad3f1d2c7b70db6367e21b
Jason Watson <jwatsonian@gmail.com>
2016-03-04 17:15:07 -0800

    First upload

---
+++ smartapps/watsonian-institute/unlock-door-deactivates-alarm.src/unlock-door-deactivates-alarm.groovy
@@ -0,0 +1,50 @@
+/**
+ *  Unlock Door Deactivates Alarm
+ *
+ *  Copyright 2016 Jason Watson
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+definition(
+    name: "Unlock Door Deactivates Alarm",
+    namespace: "watsonian-institute",
+    author: "Jason Watson",
+    description: "Unlocking door with Smart Lock enabled Kwikset door deactivates the alarm",
+    category: "Safety & Security",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+
+
+preferences {
+	section("Title") {
+		// TODO: put inputs here
+	}
+}
+
+def installed() {
+	log.debug "Installed with settings: ${settings}"
+
+	initialize()
+}
+
+def updated() {
+	log.debug "Updated with settings: ${settings}"
+
+	unsubscribe()
+	initialize()
+}
+
+def initialize() {
+	// TODO: subscribe to attributes, devices, locations, etc.
+}
+
+// TODO: implement event handlers
